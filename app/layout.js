b3ce91fb0c3488251af8feef2af1062021856467
Tahirali <tahiralimasi101@gmail.com>
2026-05-17 22:09:28 +0530

    prepare

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
 
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
-              <p>Made with 💗 by RoadsideCoder</p>
+              <p>Made by Tahirali Masi 💗</p>
             </div>
           </footer>
         </body>
