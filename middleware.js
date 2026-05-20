a82d8f26ce60043cbf766e5ce1a6274ad680fd27
Tahirali <tahiralimasi101@gmail.com>
2026-05-20 19:39:10 +0530

    fix clerk middleware

--- middleware.js
+++ middleware.js
@@ -1,26 +1,11 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/account(.*)",
-  "/transaction(.*)",
-]);
-
-export default clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth();
-
-  if (!userId && isProtectedRoute(req)) {
-    const { redirectToSignIn } = await auth();
-    return redirectToSignIn();
-  }
-
-  return NextResponse.next();
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
     "/((?!_next|.*\\..*).*)",
+    "/",
     "/(api|trpc)(.*)",
   ],
 };
